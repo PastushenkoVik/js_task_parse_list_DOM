f821d1c11da1a1b0763392911855b05d58843397
Viktor Pastushenko <viktor.pastushenko@udelphi.com>
2025-05-07 13:52:45 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const sortList = (list) => {
+  const salaryToInt = (listItem) =>
+    listItem.getAttribute('data-salary').replace(/[$,]/g, '');
+
+  return [...list]
+    .sort((a, b) => b.innerText.localeCompare(a.innerText))
+    .sort((a, b) => salaryToInt(a) - salaryToInt(b));
+};
+
+const getEmployees = (list) =>
+  [...list].reduce((persons, item) => {
+    persons.push({
+      name: item.innerText,
+      position: item.getAttribute('data-position'),
+      salary: item.getAttribute('data-salary'),
+      age: item.getAttribute('data-age'),
+    });
+
+    return persons;
+  }, []);
+
+const ulTag = document.getElementsByTagName('ul').item(0);
+
+sortList(document.querySelectorAll('[data-salary]')).forEach((employee) => {
+  ulTag.prepend(employee);
+});
+
+getEmployees(document.querySelectorAll('[data-salary]'));
